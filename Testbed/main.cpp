e92cc071f6083b11665a95c5e5f56b4dee953da9
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2024-09-06 00:09:49 -0700

    Seperated lines that were previously merged All UART devices were on the same bus and have since been moved to GPS_UART and RADIO_UART Motor UART has been replaced with I2C NRST lines have been seperated out for motor/radio

--- Testbed/main.cpp
+++ Testbed/main.cpp
@@ -11,6 +11,20 @@
 #define BLINKING_RATE     500ms
 
 
+I2C i2c(PB_7, PB_8);
+void scanI2C(USBSerial* pc) {
+    pc->printf("Starting I2C Scan\n");
+    int ack;
+    int address;
+    for(address=1;address<256;address++) {    
+        ack = i2c.write(address, "11", 1);
+        if (ack == 0) {
+            pc->printf("\tFound at %3d -- %3x\r\n", address>>1,address>>1);
+        }    
+    } 
+    pc->printf("I2C Scan Finished\n");
+}
+
 int main() {
     DigitalOut led(PA_15);
     USBSerial pc(false, 0x1F00, 0x2012);
