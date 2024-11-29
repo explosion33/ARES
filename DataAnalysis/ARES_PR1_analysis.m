71f84d0090725900020c0679f8eecfe22b4b95f5
Jimmy Fowler <118411457+jimmyfowler@users.noreply.github.com>
2024-11-28 23:06:31 -0800

    added gsim testing folder and did some sim stuff

--- DataAnalysis/ARES_PR1_analysis.m
+++ DataAnalysis/ARES_PR1_analysis.m
@@ -1,29 +1,33 @@
 %%% ARES PR Launch Data Analysis %%%
 
+
 %% get data
 data = readtable("data/Pocket_Rocket_ARES_Data.csv");
 
 % remove NaNs from data
 gpsData = rmmissing( data(:, {'GPSLat', 'GPSlong', 'GPSAlt'}));
-lat = gpsData{:, 'GPSLat'};
-long = gpsData{:, 'GPSlong'};
-alt = gpsData{:, 'GPSAlt'};
+gpsLat = gpsData{:, 'GPSLat'};
+gpsLong = gpsData{:, 'GPSlong'};
+gpsAlt = gpsData{:, 'GPSAlt'};
+
+t = data{:, 'SystemMillis'}(:,1)/1000; % convert miliseconds to seconds
 
 % check to make sure data is the same length
-if size(lat) == size(long)
-    disp(['size: ', string(size(lat))])
+if size(gpsLat) == size(gpsLong)
+    disp(['size: ', string(size(gpsLat))])
     disp('wahoo')
 else
-    disp('waaaaaaaa')
+    disp('wAAAaaaaa')
 end
+
 
 %% plot 2D GPS trajectory
 figure;
 
 hold on;
-plot(long, lat, 'k.-', 'MarkerSize', 20)
-plot(long(1), lat(1), 'g.-', 'MarkerSize', 20)
-plot(long(end), lat(end), 'r.-', 'MarkerSize', 20)
+plot(gpsLong, gpsLat, 'k.-', 'MarkerSize', 20)
+plot(gpsLong(1), gpsLat(1), 'g.-', 'MarkerSize', 20)
+plot(gpsLong(end), gpsLat(end), 'r.-', 'MarkerSize', 20)
 hold off;
 
 % label 2D plot
@@ -33,11 +37,13 @@ ylabel('lattitude');
 grid on
 savefig('figs/ARES_2d_traj')
 
+
 %% geoplot
 figure;
-geoplot(lat, long, 'k.-', lat(1), long(1), 'g.', MarkerSize=15)
+geoplot(gpsLat, gpsLong, 'k.-', gpsLat(1), gpsLong(1), 'g.', MarkerSize=15)
 title('ARES 2D Flight Trajectory (GPS)')
 savefig('figs/ARES_2d_geoplot_traj')
+
 
 %% plot 3D GPS trajectory
 figure;
@@ -48,14 +54,14 @@ figure;
 % zlabel('altitude');
 
 % use haversine to change lat long into meters fromm start position
-N = length(lat);
+N = length(gpsLat);
 coordsConstLong = NaN(N, 2);
 coordsConstLat = NaN(N, 2);
 coordsStart = NaN(N, 2);
 for i=1:N
-    coordsConstLat(i,:) = [lat(1) long(i)];
-    coordsConstLong(i,:) = [lat(i) long(1)];
-    coordsStart(i,:) = [lat(1) long(1)];
+    coordsConstLat(i,:) = [gpsLat(1) gpsLong(i)];
+    coordsConstLong(i,:) = [gpsLat(i) gpsLong(1)];
+    coordsStart(i,:) = [gpsLat(1) gpsLong(1)];
 end
 
 x_long = NaN(N, 1);
@@ -65,7 +71,7 @@ for i=1:N
     y_lat(i) = haversine(coordsStart(i,:), coordsConstLong(i,:));
 end
 
-plot3(x_long, y_lat, alt, 'b.-') 
+plot3(x_long, y_lat, gpsAlt, 'b.-') 
 xlabel('x distance (m)'); % longitudinal 
 ylabel('y distance (m)'); % lattitudinal
 zlabel('altitude (m)'); 
@@ -76,37 +82,66 @@ grid on
 
 savefig('figs/ARES_3d_traj')
 
-%% get 3D trajectory w/ accelerometer data
-dataIMU = 1;
+
+%% get 3D trajectory w/ accelerometer data (IN PROGRESS)
+accel = data{:, {'BNOAccelX', 'BNOAccelY', 'BNOAccelZ'}};
+% accel(:,1) = accel(:,1) - data{:, {'BNO_Grav_X'}}; % idk what to do with the gravity
+
+% initialize position and velocity = [0, 0, 0]
+N = length(accel);
+pos = NaN(N, 3);
+pos(1, :) = [0 0 0];
+vel = NaN(N, 3);
+vel(1, :) = [0 0 0];
+
+% use projectile motion equations
+for i=1:N-1
+    dt = t(i+1)-t(i);
+    vel(i+1,:) = vel(i,:) + accel(i,:)*dt;
+    pos(i+1,:) = pos(i,:) + vel(i,:)*dt + accel(i,:)*dt^2;
+end
+
+figure;
+plot(t, pos(:,3))
 
 
 %% analyze vertical velocity w/ BME data
-t = data{:, 'SystemMillis'}(:,1);
 bmeAlt = data{:, 'BMEApproximateAlt'}(:,1);
+
+figure('Position',[10 10 600 800]);
+
+% plot altitude over time
+subplot(2,1,1)
+plot(t/60, bmeAlt, 'r-');
+xlabel('time (min)')
+ylabel('approx. altitude (m)')
+title('ARES Altitude vs. Time')
+xlim([0 16])
+grid on
 
 % calculate descent velocity (v = dz/dt)
 N = length(bmeAlt);
 v_z = NaN(N, 1);
-nEuler = 200;
+nEuler = 1;
 for i = 1:N-nEuler
-    v_z(i) = ( bmeAlt(i+nEuler)-bmeAlt(i) ) / ( (t(i+nEuler)-t(i))/1000 );
+    v_z(i) = ( bmeAlt(i+nEuler)-bmeAlt(i) ) / ( (t(i+nEuler)-t(i)) );
     % avg descent rate over the next 100 samples 
 end
 v_z(end) = 0;
 disp(v_z(:,1)) 
 
-% % implement moving average filter
-% dt = 500; % ms
-% sampleAvg = 50; % use avg across 50 samples (25 sec)
-% coeff = ones(1, sampleAvg)/sampleAvg; 
-% v_mvngAvg = filter(coeff, 1, v_z);
-% fDelay = (length(coeff)-1)/2; % filter delay
+% implement moving average filter
+dt = 500; % ms
+sampleAvg = 100; % use avg across 50 samples (25 sec)
+coeff = ones(1, sampleAvg)/sampleAvg; 
+v_mvngAvg = filter(coeff, 1, v_z);
+fDelay = (length(coeff)-1)/2; % filter delay
 
 % plot descent velocity over time
-figure();
+subplot(2,1,2)
 hold on;
-plot(t/60000,v_z, LineWidth=1.5)
-% plot((t-fDelay)/60000,v_mvngAvg, LineWidth=2)
+% plot(t/60, v_z, LineWidth=1)
+plot((t-fDelay)/60,v_mvngAvg, 'b-', LineWidth=1)
 
 grid on
 
@@ -115,8 +150,8 @@ hAx=gca;
 hAx.YDir='reverse';
 
 % label plot
-title('ARES Descent Rate Profile')
 xlabel('time (min)')
 ylabel('approx. descent rate (m/s)')
+title('ARES Descent Rate vs. Time')
 
-savefig('figs/ARES_descent_rate')
+savefig('figs/ARES_descent_rate') 
---
+++ Software/Simulation/dynamics.m
@@ -0,0 +1,20 @@
+%%% Building System Dynamics and Transfer Functions %%%
+
+
+% put linear ODE describing system angular dynamics here
+
+% horizontal:
+
+% verticle:
+
+
+% transfer functions
+
+G_h = tf(1,[3 2 1]);    % horizontal transfer function
+                        % asymmetric deflection --> ang. velocity
+
+G_v = tf(1,[2 1 7]);    % verticle transfer function
+                        % symmetric deflection --> decent rate
+
+% might run into issues bc decent rate initial condition is not zero
+
