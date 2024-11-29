571d357fe5220b6e56e9409f67c07d19088c830b
Jimmy Fowler <jfowler0@uw.edu>
2024-11-29 01:18:03 -0800

    plot input break and output ang. velocity

--- Software/glidersim-testing/prebuilt_example.py
+++ Software/glidersim-testing/prebuilt_example.py
@@ -2,8 +2,7 @@
 
 import pfh.glidersim as gsim
 import scipy.interpolate
-import matplotlib.pyplot as plt
-import numpy as np
+
 
 # Component models: use a pre-built wing and add a suitable harness
 wing = gsim.extras.wings.niviuk_hook3(size=23, verbose=False)
@@ -21,12 +20,9 @@ paraglider = gsim.paraglider.ParagliderSystemDynamics6a(wing, harness)
 # Build a control input sequence for right brake using linear interpolation:
 # the input is 0 until t=3, increases to 1 over t=3..5, holds at 1 until t=22,
 # decreases to zero over t=22..23, and holds at 0 indefinitely.
-t = np.array([0, 3, 10])   # times
-br = np.array([0, 1, 1]) # fraction (1 = 100%)
-
 delta_br = scipy.interpolate.interp1d(
-    t,      # times
-    br,     # fraction (1 = 100%)
+    [0, 3, 5, 22, 23],  # times
+    [0, 0, 1, 1, 0],  # fraction (1 = 100%)
     fill_value=0,
     bounds_error=False,
 )
@@ -47,25 +43,9 @@ model = gsim.simulator.ParagliderStateDynamics6a(paraglider, **sim_parameters)
 # Setup and run the simulation
 state0 = model.starting_equilibrium()  # Start the simulation at equilibrium
 dt = 0.5  # Record the state every 0.5 seconds
-T = 10  # Run for 10 seconds
-
+T = 25  # Run for 25 seconds
 times, states = gsim.simulator.simulate(model, state0, dt=dt, T=T)
-
-omega_z = np.array( states['omega_b2e'][:,2] )
-print(omega_z)
-
-fig, ax = plt.subplots(2, sharex=True)
-ax[0].plot(t, br*100, 'b-')
-ax[0].set_ylabel('right break deflection (%)')
-ax[1].plot(times, omega_z*(360/(2*np.pi)), 'r-') # turn rad/s into deg/s
-ax[1].set_ylabel('angular velocity (deg/s)')
-ax[1].set_xlabel('time (s)')
-ax[0].grid()
-ax[1].grid()
-plt.show()
 
 # 3D plot: position over time
 points = gsim.extras.simulation.sample_paraglider_positions(model, states, times)
-# print(type(points), points)
-
-# gsim.extras.plots.plot_3d_simulation_path(**points)
+gsim.extras.plots.plot_3d_simulation_path(**points)
