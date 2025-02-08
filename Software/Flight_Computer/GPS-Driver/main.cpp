c68c0e6e82c93d14104bc50c44e9446db4ed5600
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2025-02-07 21:32:39 -0800

    Implemented starting point for GPS Driver Fixed missing file issues in blackpill build target Added EUSBSerial class for better USBSerial support Updated main.cpp with working examples

--- Software/Flight_Computer/GPS-Driver/main.cpp
+++ Software/Flight_Computer/GPS-Driver/main.cpp
@@ -1,25 +1,33 @@
 #include "mbed.h"
-#include "USBSerial.h"
+#include "EUSBSerial.h"
 
 int main(){
     DigitalOut led_G(PA_15);
     DigitalOut led_B(PA_8);
 
-    USBSerial pc(true);
-    BufferedSerial gps(PA_3, PA_2, 9600);
+    EUSBSerial pc(true); // new class Extended USB Serial
+    BufferedSerial gps(PA_2, PA_3);
 
-    ThisThread::sleep_for(10s);
-    // led_G.write(1);
+    led_B.write(1);
+    
+    // setup timer for use in secondary actions
+    Timer t;
+    t.start();
+
+    char c;
     while(true){
-
-        if ( gps.readable() ){
-            char c;
+        // if gps has characters, read into c and write it to USB
+        if (gps.readable()){ 
             gps.read(&c, 1);
-            // pc._putc(c);
-
-            led_B.write(1);
+            pc.write(&c, 1);
         }
 
+        // every 5s & if GPS line has been sent toggle LED & print to USB
+        if (t.read_ms() >= 5000 && c == '\n') {
+            pc.printf("task #%d\n", 2);
+            led_B = !led_B;
+            t.reset();
+        }
     }
 }
 
