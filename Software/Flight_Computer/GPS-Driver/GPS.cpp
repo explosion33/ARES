018afa2bf94f0963733c14aa3f9c50a06d8f8e5a
Jimmy Fowler <118411457+jimmyfowler@users.noreply.github.com>
2025-02-24 20:03:36 -0800

    added getPosLTP function (not tested)

--- Software/Flight_Computer/GPS-Driver/GPS.cpp
+++ Software/Flight_Computer/GPS-Driver/GPS.cpp
@@ -1,6 +1,8 @@
 #include "EUSBSerial.h"
 #include "GPS.h"
+#include <cmath>
 
+GPS::GPS() {}
 
 gpsState GPS::getState() const{
     return state; // return a copy of the state (can't be modified bc its private)
@@ -117,4 +119,83 @@ void GPS::update(int msgType, const char* msg){
             break;
         }
     }
+
+}
+
+// posECEFg GPS::getPosECEFg() { 
+
+//     posECEFg z = state.lat
+
+//     return posECEFg;
+// }
+
+posLTP GPS::getPosLTP() {
+    // state.lat = ddmm.mmmm ... state.lat/100 = dd.mmmmmm
+    // Determine lat/lon coords as North or South & East or West
+    int lat_sign;
+    int lon_sign;
+    switch(state.latNS) {
+        case('N'): {
+            lat_sign = 1;
+            break;
+        }
+        case('S'): {
+            lat_sign = -1;
+            break;
+        }
+        printf("something is super wrong... latNS = '%c'", state.latNS);
+    }
+    switch(state.lonEW) {
+        case('E'): {
+            lon_sign = 1;
+            break;
+        }
+        case('W'): {
+            lon_sign = -1;
+            break;
+        }
+        printf("something is super wrong... latEW = '%c'", state.lonEW);
+    }
+
+    double pi = 3.1415926535898;
+
+    // get longitude and latitude into radians
+    int lat_deg = floor(state.lat/100);
+    double lat_min = fmod(state.lat, 100);
+    double lat_rad = lat_sign*( (lat_deg + lat_min/60) * pi/180 );
+
+    int lon_deg = floor(state.lat/1000);
+    double lon_min = fmod(state.lat, 1000);
+    double lon_rad = lon_sign*( (lon_deg + lon_min/60) * pi/180 );
+
+    
+    double a = 6378137; // earth semi-major axis        (m)
+    double b = 6356752.3142; // earth semi-minor axis   (m)
+    double f = (a-b)/a; // ellipsoid flatness 
+    double e = sqrt(f*(2-f)); // eccentricity
+    // distance from the earth's surface to the z-axis along the ellipsoid normal
+    double N = a/sqrt( 1 - pow(e, 2)* pow(sin(lat_rad), 2) ); 
+
+    // get current position in ECEF-r coordinates
+    double h = state.alt;
+    double x = (h + N)*cos(lat_rad)*cos(lon_rad);
+    double y = (h + N)*cos(lat_rad)*sin(lon_rad);
+    double z = (h + N*(1 - pow(e,2)))*sin(lat_rad);
+    
+    // subtract the origin ECEF-r coordinate to get relative position vector
+    double x_p = x - origin.x;
+    double y_p = y - origin.y;
+    double z_p = z - origin.z;
+
+    // rotate to allign y-axis w/ LTP
+    double x_pp = -x_p*sin(lon_rad) + y_p*cos(lon_rad);
+    double y_pp = x_p*cos(lon_rad) + y_p*sin(lon_rad);
+    double z_pp = z_p;
+
+    // final rotation to allign z_axis with up
+    pos.e = x_pp;
+    pos.n = -y_pp*sin(lat_rad) + z_pp*cos(lat_rad);
+    pos.u = y_pp*cos(lat_deg) + z_pp*sin(lat_rad);
+
+    return pos;
 }
--- Software/Flight_Computer/GPS-Driver/GPS.h
+++ Software/Flight_Computer/GPS-Driver/GPS.h
@@ -3,6 +3,28 @@
 
 #include "mbed.h"
 
+
+struct posECEFg {   // Earth-Centered Earth-Fixed Geodic Coordinates
+    double lat; // lattitude    (radians)
+    double lon; // longitude    (radians)
+    double alt; // altitude     (meters)
+};
+
+
+
+struct originECEFr {   // Earth-Centered Earth-Fixed Rectangular Coordinates
+    double x;       // get origin of linear tangent plan in ECEF-r coordinates
+    double y;       // (will likely be the launch pad)
+    double z;
+};
+
+struct posLTP { // Local Tangent Plane Coordinates
+    double e;   // east     (m)
+    double n;   // north    (m)
+    double u;   // up       (m)
+};
+
+
 struct gpsState{
     double lat;
     double lon;
@@ -28,12 +50,22 @@ typedef enum {
 class GPS {
     private:
         gpsState state;
+        posLTP pos;
+        originECEFr origin;
+        double dms2rad();
+        
+        // getMsgType()
+        // update()
     
     public:
         GPS();
         gpsState getState() const;
         NMEA_Type getMsgType(const char* msg);
         void update(int msgType, const char* msg);
+        posLTP getPosLTP();
+        // functino: start (GPS LOOOP)
+        // initialize buffered serial object
+        // make wrapper function for getMsgType() & update()
 };
 
 #endif // GPS_H
