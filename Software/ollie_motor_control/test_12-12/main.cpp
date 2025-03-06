dbcaffeb0962f0a746be9276dccd0c6c06317981
ollieb393 <ollieb393@gmail.com>
2025-03-05 19:20:41 -0800

    motor control PID
    Created motor controller pid class and created an example in main

--- Software/ollie_motor_control/test_12-12/main.cpp
+++ Software/ollie_motor_control/test_12-12/main.cpp
@@ -1,6 +1,7 @@
 #include "mbed.h"
 #include <string>
-#include "Motor.cpp"
+#include "Motor.h"
+#include "PID.h"
 
 // Define the pins for the encoder
 // #define PIN_A PA_8 
@@ -10,104 +11,27 @@
 // #define MOTOR_3 PB_15
 // #define MOTOR_4 PB_14
 
-// Motor motor(PIN_A, PIN_B, PA_11, MOTOR_2, MOTOR_3, MOTOR_4);
-Motor motor(PB_3, PB_5, PA_11, PA_12, PA_10, PA_9);
+// Motor motor(PA_8, PA_10, PB_2, PB_1, PB_15, PB_14);
+Motor motor1(PB_3, PB_5, PA_11, PA_12, PA_10, PA_9);
+Motor motor2(PA_6, PA_5, PB_14, PB_15, PB_13, PA_8);
+// Motor angles
+float theta1 = 0;
+float theta2 = 0;
+
+PID pid(500, 0, 0);
 
 // TODO: void toDegrees
-
-/* DISCONTINUED
-void encoderCallback() {
-    int aCurrent = encoderA.read();
-    int bCurrent = encoderB.read();
-    counter++;
-
-    if (aCurrent != lastA) {
-        if (aCurrent == HIGH) { // Rising edge of A
-            if (bCurrent == LOW) {
-                encoderCounter++; // Clockwise
-            } else {
-                encoderCounter--; // Counterclockwise
-            }
-        } else { // Falling edge of A
-            if (bCurrent == HIGH) {
-                encoderCounter++; // Clockwise
-            } else {
-                encoderCounter--; // Counterclockwise
-            }
-        }
-        lastA = aCurrent;
-    } else {
-        counter += 1000;
-    }
-
-    if (bCurrent != lastB) {
-        if (bCurrent == HIGH) { // Rising edge of B
-            if (aCurrent == HIGH) {
-                encoderCounter++; // Clockwise
-            } else {
-                encoderCounter--; // Counterclockwise
-            }
-        } else { // Falling edge of B
-            if (aCurrent == LOW) {
-                encoderCounter++; // Clockwise
-            } else {
-                encoderCounter--; // Counterclockwise
-            }
-        }
-        lastB = bCurrent;
-    } else {
-        counter -= 1000;
-    }
-} */
-// ETHAN'S METHOD
-
-/*/ Callback global
-bool fired = false; //idk which to start it at, I think 
-                    // this works assuming a fires first
-
-void aRiseCallback() {
-    counter++;
-
-    if (!fired) {
-        fired = true;
-        encoderCounter++;
-    } else {
-        fired = false;
-        encoderCounter--;
-    }
-}
-void bRiseCallback() {
-    counter++;
-
-    if (fired) {
-        fired = false;
-        encoderCounter++;
-    } else {
-        fired = true;
-        encoderCounter--;
-    }
-}//*/
-
 
 // main() runs in its own thread in the OS
 int main()
 {
     printf("start\n");
-    motor.motorPower(0);
-    ThisThread::sleep_for(1000ms);
     DigitalOut led1(PC_14);
-    // DigitalOut led2
+    led1.write(0);
+
     while (true) {
-        printf("main\n");
-        led1.write(1);
-        motor.motorPower(-1);
-
-
-        ThisThread::sleep_for(500ms);
-        printf("degrees: %d \n", motor.getDegrees());
-        // printf("rotations: %d\n", (int)rotations);
-        // printf("encoder a: %d\n", encoderA.read());
-        // printf("encoder b: %d\n\n", encoderB.read());
+        ThisThread::sleep_for(10ms);
+        motor1.motorPower(pid.compute(motor1.getDegrees(), 720, 10));
     }
 }
 
